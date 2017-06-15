e1ea4e383ef3c688aed699dbb2608f9f6dc30a5f
Johan Oakes <oakejp12@wfu.edu>
2017-06-14 22:41:25 -0400

    Messed up favicon dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express       = require('express');
 const bodyParser    = require('body-parser');
 const http          = require('http');
 const path          = require('path');
-const favicon       = require('favicon');
+const favicon       = require('serve-favicon');
 
 // Set up the Express application
 var app = express();
@@ -16,7 +16,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon')));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 
 require(path.join(__dirname, 'routes', 'html-routes.js'))(app);
 
