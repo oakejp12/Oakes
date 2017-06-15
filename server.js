e20acc6a9101631df1edbf930c2f08b58039b5d8
Johan Oakes <oakejp12@wfu.edu>
2017-06-14 22:37:52 -0400

    Favicon insertion

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('http');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const http          = require('http');
+const path          = require('path');
+const favicon       = require('favicon');
 
 // Set up the Express application
 var app = express();
@@ -13,9 +15,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon')));
 
-require('./routes/html-routes.js')(app);
+require(path.join(__dirname, 'routes', 'html-routes.js'))(app);
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
