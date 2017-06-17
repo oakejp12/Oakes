e13910dc44bb9ecc0994179af799d4bd9ca0d3ed
Johan Oakes <oakejp12@wfu.edu>
2017-06-17 16:49:09 -0400

    Keys don't have to be in strings

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "gh_username": process.env.GITHUB_USERNAME,
-    "gh_creds": process.env.GITHUB_TOKEN,
+    gh_username: process.env.GITHUB_USERNAME,
+    gh_creds: process.env.GITHUB_TOKEN,
 }
 
