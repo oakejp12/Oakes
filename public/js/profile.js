c888df0c2bf234e2c7977e89e64ca3eaea86cc20
Johan Oakes <oakejp12@wfu.edu>
2017-08-17 20:29:00 -0400

    App dependencies so Heroku can recognize it

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,17 +4,23 @@
  */
 var github_data = (path, id) => {
     return $.getJSON(path, (data) => {
+        console.log(data);
+        
         const data_json = JSON.parse(data);
-        let actual_data = data_json.data;
+        
+        console.log("JSON: " + data_json);
+
+        const actual_data = data_json.data;
+        
         document.getElementById(id).innerHTML = actual_data.length;
     })
     .fail((err) => {
-        console.error("Error grabbing follower data.", err);
+        console.error("Error grabbing data.", err);
     })
 }
 
 $(document).ready(() => {
     github_data("/repos", "repos"); // Set H2 element to number of repos
     github_data("/followers", "followers"); // Set H2 element to number of followers
-    // github_data("/commits", "commits");
+    github_data("/commits", "commits");
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,26 +6,20 @@ module.exports = function(app) {
     console.log("Hitting the repo path...");
     request.grabRepoCount({
     }, (err, response) => {
-        if (err) throw new Error("Error hitting Github API: " + err);
-
-        console.log("Github response length: " + response.length);
-
-        res.json(response);
+      if (err) throw new Error("Error hitting Github API: " + err);
+      res.json(response);
     });
   });
 
   app.get("/commits", (req, res) => {
     console.log("Hitting the commit path...");
-    request.grabCommitCount({}, (err, response) => {
-      if (err) throw new Error("Error hitting Github API at " + req.path);  
-      console.dir(response);
-      // res.json(response);
-    })
+    request.grabCommitCount({
+    }, (err, response) => {
+      if (err) throw new Error("Error hitting Github API:" + err);
+      res.json(response);
+    });
   });
 
-  /**
-   * Get all followers
-   */
   app.get("/followers", (req, res) => {
     console.log("Hitting the followers path");
     request.grabFollowers({}, (err, response) => {
